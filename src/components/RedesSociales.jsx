901eaa293f3531153e48507440d2a471dacbeb28
Andres Gallego Tovar <agallegt@emeal.nttdata.com>
2023-02-26 23:26:23 -0500

    nuevo ajuste

--- src/components/RedesSociales.jsx
+++ src/components/RedesSociales.jsx
@@ -4,7 +4,7 @@ import twitter from '../assets/gorjeo.png';
 
 const RedesSociales = () => {
   return (
-    <div className='absolute bottom-36 w-full flex justify-between px-12'>
+    <div className='absolute bottom-44 w-full flex justify-between px-12'>
         <img src={facebook} className='invert w-16' alt="facebook" />
         <img src={twitter} className='invert w-16' alt="twitter" />
         <img src={instagram} className='invert w-16' alt="instagram" />
