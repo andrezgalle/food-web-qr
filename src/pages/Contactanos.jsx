ff3b246dfcf99ae74dcad2a84abc8d2d61a5439d
Andres Gallego Tovar <agallegt@emeal.nttdata.com>
2023-03-05 00:56:22 -0500

    agregando 2 secciones

--- src/pages/Contactanos.jsx
+++ src/pages/Contactanos.jsx
@@ -1,6 +1,6 @@
 import MenuHamburguesa from "../components/MenuHamburguesa"
 import { useState, useEffect } from 'react';
-import MenuScrollX from "../components/MenuScrollX";
+import CardInformation from "../components/CardInformation";
 const Contactanos = () => {
     useEffect(() => {
         const body = document.querySelector('body');
@@ -12,7 +12,26 @@ const Contactanos = () => {
         <MenuHamburguesa
           setBody={setBody}
         />
-        <p>Contáctanos</p>
+        <h1 className="mt-10 w-5/6 mx-auto font-bold uppercase text-2xl text-center">¿Donde podrás encontrarnos?</h1>
+
+        <div className="mt-5">
+          <CardInformation
+            titulo={'Don Kiko Localidad Ciudad Bolivar'}
+            direccion={'Calle 69 b sur # 18 h-65'}
+            telefonos={'3126613327'}
+            apertura={'11:AM'}
+            cierre={'11:AM'}
+            url={'https://www.google.com/maps?q=Asadero+Don+KIKO,+Calle+67c+Sur,+Bogotá'}
+          />
+          <CardInformation
+            titulo={'Don Kiko Municipio Funza'}
+            direccion={'Calle 69 b sur # 18 h-65'}
+            telefonos={'3126613327'}
+            apertura={'11:AM'}
+            cierre={'11:AM'}
+            url={'https://www.google.com/maps?q=Asadero+Don+KIKO,+Calle+67c+Sur,+Bogotá'}
+          />
+        </div>
     </>
   )
 }
--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -12,7 +12,18 @@ const Nosotros = () => {
           setBody={setBody}
         />
 
-        <h1>Nosotros</h1>
+        <div className="background mt-2">
+          <h1 className="text-4xl text-center font-semibold text-white">Conoce un poco más sobre nosotros</h1>
+        </div>
+
+        <div className="my-10 px-8">
+          <h2 className="mb-4 text-2xl font-bold">¡Bienvenidos a Don Kiko!</h2>
+          <p className="mb-4 text-lg text-justify">Somos un asadero familiar con más de 25 años de experiencia en ofrecer lo mejor de la gastronomía colombiana.</p>
+          <p className="mb-4 text-lg text-justify">En Don Kiko, ofrecemos una gran variedad de platos deliciosos, entre ellos nuestra especialidad: el pollo asado, siempre preparado con ingredientes frescos y de alta calidad, adobado con nuestras especias colombianas secretas y asado al carbón para lograr ese sabor auténtico que tanto nos gusta.</p>
+          <p className="mb-4 text-lg text-justify">Además, nos enorgullece preparar cada uno de nuestros platos con ingredientes frescos y de calidad, para ofrecerte siempre la mejor comida en un ambiente auténtico y familiar.</p>
+          <p className="mb-4 text-lg text-justify">Ven y prueba nuestros platos y descubre por qué somos un asadero de renombre en nuestra localidad.</p>
+          <p className="mb-4 text-lg text-justify"><span className="font-bold">¡Gracias por elegir Don Kiko para tu próxima comida!</span> Te esperamos con los brazos abiertos.</p>
+        </div>
     </>
   )
 }
